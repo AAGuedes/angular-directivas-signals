30d500dac1255a73c87770ceb10730433e592381
AAGuedes <airam.a.g.85@gmail.com>
2023-06-04 16:38:06 +0100

    Menú lateral

--- src/app/signals/signals.module.ts
+++ src/app/signals/signals.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { CounterPageComponent } from './pages/counter-page/counter-page.component';
+import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { SignalsLayoutComponent } from './layout/signals-layout/signals-layout.component';
 import { SignalsRoutingModule } from './signals-routing.module';
-import { CounterPageComponent } from './pages/counter-page/counter-page.component';
 import { UserInfoPageComponent } from './pages/user-info-page/user-info-page.component';
-import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
 
 
 @NgModule({
   declarations: [
-    SignalsLayoutComponent,
     CounterPageComponent,
+    PropertiesPageComponent,
+    SideMenuComponent,
+    SignalsLayoutComponent,
     UserInfoPageComponent,
-    PropertiesPageComponent
   ],
   imports: [
     CommonModule,
