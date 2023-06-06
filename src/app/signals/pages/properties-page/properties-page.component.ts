2745181af5105c0ddbda3dc7e07480da0d5a2542
AAGuedes <airam.a.g.85@gmail.com>
2023-06-06 08:27:28 +0100

    Mutaciones

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, WritableSignal, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   selector: 'app-properties-page',
@@ -7,7 +8,46 @@ import { Component } from '@angular/core';
 })
 export class PropertiesPageComponent {
 
-  onFieldUpdated(field: string, value: string): void {
-    console.log({ field, value});
+  public user: WritableSignal<User> = signal<User>({
+    id: 1,
+    email: "george.bluth@reqres.in",
+    first_name: "George",
+    last_name: "Bluth",
+    avatar: "https://reqres.in/img/faces/1-image.jpg"
+  });
+
+  onFieldUpdated(field: keyof User, value: string): void {
+    this.user.mutate(mutate => {
+      switch(field) {
+        case 'id':
+          mutate.id = Number(value);
+          break;
+        case 'email':
+          mutate.email = value;
+          break;
+        case 'first_name':
+          mutate.first_name = value;
+          break;
+        case 'last_name':
+          mutate.last_name = value;
+          break;
+        case 'avatar':
+          mutate.avatar = value;
+          break;
+      }
+    });
+
+    // this.user.update(update => {
+    //   return {
+    //     ...update,
+    //     [field]: value
+    //   };
+    // });
+
+    // this.user.set({
+    //   ...this.user(),
+    //   [field]: value,
+    // });
   }
+
 }
